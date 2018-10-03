815e1ce183754e891ef1a16afdf971f549fc3352
kxhuan <canyonh@gmail.com>
2018-10-03 17:15:13 +1000

    add stacktrace

--- Makefile
+++ Makefile
@@ -1,14 +1,14 @@
-memtest.o:
-	gcc -Wall -fPIC -c memtest.c -o memtest.o
+all: libmemtest.so.1.0 test
+
+memtest.o: memtest.c
+	gcc -ggdb -Wall -I/usr/include/c++/5 -fPIC -c memtest.c -o memtest.o
 
 libmemtest.so.1.0: memtest.o
-	gcc -shared -Wl,-soname,libmemtest.so.1.0 -o libmemtest.so.1.0 memtest.o
+	gcc -shared -rdynamic -ldl -Wl,-soname,libmemtest.so.1.0 -o libmemtest.so.1.0 memtest.o
 	ln -s ./libmemtest.so.1.0 libmemtest.so
 
-test: libmemtest.so.1.0
-	g++ -L. main.cpp -o test -lmemtest
-
-all: libmemtest.so.1.0 test
+test: main.cpp libmemtest.so.1.0
+	g++ -ggdb -L. main.cpp -o test -lmemtest -pthread -rdynamic -ldl
 	
 clean:
 	rm -f *.o libmemtest.*
--- memtest.c
+++ memtest.c
@@ -1,4 +1,9 @@
+#define _GNU_SOURCE
+#include <dlfcn.h>
+
 #include <stdio.h>
+#include <execinfo.h>
+#include <cxxabi.h>
 
 extern void* __libc_malloc(size_t);
 extern void* __libc_free(void*);
@@ -29,6 +34,35 @@ static void __attribute((constructor)) init(void)
 
 static __thread int no_hook = 0;
 
+void print_trace(void)
+{
+	void *array[10];
+	size_t size = backtrace(array, 10);
+	char** strings = backtrace_symbols(array, size);
+
+	size_t i;
+	Dl_info dlinfo;
+	printf("-------------------------\n");	
+	for (i = 0; i < size; ++i)
+	{
+		const char* function_name = NULL;
+		if (dladdr(array[i], &dlinfo) != 0)
+		{
+			function_name = dlinfo.dli_sname;
+			if (function_name != NULL)
+			{
+				const char* demangled_name = NULL;
+				demangled_name = abi:__cxa_demangle(function_name, 0, 0, &status);
+				if (status == 0)
+					function_name = de_mangled_name;
+			}
+		}
+
+		printf("%d %s %s\n", (int)i, strings[i], function_name);
+	}
+	printf("-------------------------\n");	
+}
+
 void* malloc(size_t len)
 {
 	if (no_hook)
@@ -37,13 +71,17 @@ void* malloc(size_t len)
 	no_hook = 1;
 	void* p = (*__libc_malloc)(len);
 	printf("+%p:%d\n", p, (int)len);
+	print_trace();	
 	no_hook = 0;
 	return p;
 }
 
 void free(void*p)
 {
+	no_hook = 1;
 	printf("-%p\n", p);
+	print_trace();	
+	no_hook = 0;
 	__libc_free(p);
 }
 
